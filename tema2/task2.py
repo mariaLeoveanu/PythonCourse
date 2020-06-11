acc038926cd222786c88fe90b20fb6c9fb5ee0fc
mariaLeoveanu <35270676+mariaLeoveanu@users.noreply.github.com>
2020-06-11 13:58:37 +0300

    reorganize directories

--- tema2/task2.py
+++
@@ -1,9 +0,0 @@
-number = input("Introduceti un numar: ")
-if not number.isdigit():
-    print("Nu ati introdus un numar.")
-else:
-    number = int(number)
-    if number % 2 == 0:
-        print("Numarul este par.")
-    else:
-        print("Numarul este impar.")
--- tema2/task3.py
+++
@@ -1,9 +0,0 @@
-an = input("Introduceti un an: ")
-if not an.isdigit():
-    print("Nu ati introdus un numar")
-else:
-    an = int(an)
-    if an % 4 == 0:
-        print("Anul este bisect.")
-    else:
-        print("Anul nu este bisect.")
